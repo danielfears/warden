4f79653aacd72013d018a39ecf46621553a4ef58
Daniel Fears <danieljfears@gmail.com>
2018-10-01 16:02:13 +0100

    Added unset functionality to source command

--- assets/warden-source.sh
+++ assets/warden-source.sh
@@ -19,6 +19,10 @@ if [ ! -f ~/opt/warden/$username-creds.sh.gpg ]; then
     echo "Username not found!"
     echo ""
 else
+    unset ARM_SUBSCRIPTION_ID
+    unset ARM_CLIENT_ID
+    unset ARM_CLIENT_SECRET
+    unset ARM_TENANT_ID
     gpg --output ~/opt/warden/$username-creds.sh --decrypt ~/opt/warden/$username-creds.sh.gpg
     wait
     source ~/opt/warden/$username-creds.sh

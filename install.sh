29eeac30a50f42d0b5606bc9e1678cc7fa5c439d
Daniel Fears <danieljfears@gmail.com>
2018-12-13 12:59:42 +0000

    Alias path changed to usr local bin

--- install.sh
+++ install.sh
@@ -21,12 +21,12 @@ if [ ! -d /usr/local/bin/warden ]; then
   # Ubuntu installation support
   if [ -e ~/.bashrc ] && [ ! -e ~/.bash_profile ]; then
 
-    echo 'alias warden-add=". ~/opt/warden/warden-add.sh"' >> ~/.bashrc
-    echo 'alias warden-help=". ~/opt/warden/warden-help.sh"' >> ~/.bashrc
-    echo 'alias warden-list=". ~/opt/warden/warden-list.sh"' >> ~/.bashrc
-    echo 'alias warden-remove=". ~/opt/warden/warden-remove.sh"' >> ~/.bashrc
-    echo 'alias warden-source=". ~/opt/warden/warden-source.sh"' >> ~/.bashrc
-    echo 'alias warden-uninstall=". ~/opt/warden/warden-uninstall.sh"' >> ~/.bashrc
+    echo 'alias warden-add=". ~/usr/local/bin/warden/warden-add.sh"' >> ~/.bashrc
+    echo 'alias warden-help=". ~/usr/local/bin/warden/warden-help.sh"' >> ~/.bashrc
+    echo 'alias warden-list=". ~/usr/local/bin/warden/warden-list.sh"' >> ~/.bashrc
+    echo 'alias warden-remove=". ~/usr/local/bin/warden/warden-remove.sh"' >> ~/.bashrc
+    echo 'alias warden-source=". ~/usr/local/bin/warden/warden-source.sh"' >> ~/.bashrc
+    echo 'alias warden-uninstall=". ~/usr/local/bin/warden/warden-uninstall.sh"' >> ~/.bashrc
     
     . ~/.bashrc
 
@@ -35,12 +35,12 @@ if [ ! -d /usr/local/bin/warden ]; then
   # CentOS installation support
   if [ -e ~/.bashrc ] && [ -e ~/.bash_profile ]; then
 
-    echo 'alias warden-add="sudo sh ~/opt/warden/warden-add.sh"' >> ~/.bashrc
-    echo 'alias warden-help="sudo sh ~/opt/warden/warden-help.sh"' >> ~/.bashrc
-    echo 'alias warden-list="sudo sh ~/opt/warden/warden-list.sh"' >> ~/.bashrc
-    echo 'alias warden-remove="sudo sh ~/opt/warden/warden-remove.sh"' >> ~/.bashrc
-    echo 'alias warden-source="sudo sh ~/opt/warden/warden-source.sh"' >> ~/.bashrc
-    echo 'alias warden-uninstall="sudo sh ~/opt/warden/warden-uninstall.sh"' >> ~/.bashrc
+    echo 'alias warden-add="sudo sh ~/usr/local/bin/warden/warden-add.sh"' >> ~/.bashrc
+    echo 'alias warden-help="sudo sh ~/usr/local/bin/warden/warden-help.sh"' >> ~/.bashrc
+    echo 'alias warden-list="sudo sh ~/usr/local/bin/warden/warden-list.sh"' >> ~/.bashrc
+    echo 'alias warden-remove="sudo sh ~/usr/local/bin/warden/warden-remove.sh"' >> ~/.bashrc
+    echo 'alias warden-source="sudo sh ~/usr/local/bin/warden/warden-source.sh"' >> ~/.bashrc
+    echo 'alias warden-uninstall="sudo sh ~/usr/local/bin/warden/warden-uninstall.sh"' >> ~/.bashrc
 
     . ~/.bashrc
 
@@ -49,12 +49,12 @@ if [ ! -d /usr/local/bin/warden ]; then
   # MacOS installation support
   if [ -e ~/.bash_profile ] && [ ! -e ~/.bashrc ]; then
 
-    echo 'alias warden-add="sudo sh ~/opt/warden/warden-add.sh"' >> ~/.bash_profile
-    echo 'alias warden-help="sudo sh ~/opt/warden/warden-help.sh"' >> ~/.bash_profile
-    echo 'alias warden-list="sudo sh ~/opt/warden/warden-list.sh"' >> ~/.bash_profile
-    echo 'alias warden-remove="sudo sh ~/opt/warden/warden-remove.sh"' >> ~/.bash_profile
-    echo 'alias warden-source="sudo sh ~/opt/warden/warden-source.sh"' >> ~/.bash_profile
-    echo 'alias warden-uninstall="sudo sh ~/opt/warden/warden-uninstall.sh"' >> ~/.bash_profile
+    echo 'alias warden-add="sudo sh ~/usr/local/bin/warden/warden-add.sh"' >> ~/.bash_profile
+    echo 'alias warden-help="sudo sh ~/usr/local/bin/warden/warden-help.sh"' >> ~/.bash_profile
+    echo 'alias warden-list="sudo sh ~/usr/local/bin/warden/warden-list.sh"' >> ~/.bash_profile
+    echo 'alias warden-remove="sudo sh ~/usr/local/bin/warden/warden-remove.sh"' >> ~/.bash_profile
+    echo 'alias warden-source="sudo sh ~/usr/local/bin/warden/warden-source.sh"' >> ~/.bash_profile
+    echo 'alias warden-uninstall="sudo sh ~/usr/local/bin/warden/warden-uninstall.sh"' >> ~/.bash_profile
 
     . ~/.bash_profile
 
